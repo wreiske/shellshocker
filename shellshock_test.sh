657d9d04c1ec7c2c74697e798a863df4786a917f
Will Reiske <wreiske@mieweb.com>
2014-09-30 14:40:08 -0400

    Added Florian's patch to the shellshock tester.
    Updated link in index.html to point to nist

--- shellshock_test.sh
+++ shellshock_test.sh
@@ -45,3 +45,12 @@ else
 	echo -e "\033[92mnot vulnerable\033[39m"
 fi
 
+#CVE-2014-6278
+
+CVE20146278=$(shellshocker='() { echo vulnerable; }' bash -c shellshocker | grep 'vulnerable' | wc -l)
+echo -n "CVE-2014-6278 (Florian's patch): "
+if [ $CVE20146278 -gt 0 ]; then
+	echo -e "\033[91mVULNERABLE\033[39m"
+else
+	echo -e "\033[92mnot vulnerable\033[39m"
+fi
