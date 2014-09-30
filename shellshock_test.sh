c59b69a0ad6a20b8bacba11387b944db82cc31c6
unknown <mhensler@paladin.ptloma.edu>
2014-09-30 09:30:47 -0700

    simplified the [[ test ]] to work in older bash shells

--- shellshock_test.sh
+++ shellshock_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # CVE-2014-6271
-CVE20146271=$(env 'x=() { :;}; echo vulnerable' 'BASH_FUNC_x()=() { :;}; echo vulnerable' bash -c "echo test" 2>&1)
+CVE20146271=$(env 'x=() { :;}; echo vulnerable' 'BASH_FUNC_x()=() { :;}; echo vulnerable' bash -c "echo test" 2>&1 | grep 'vulnerable' | wc -l)
 
-if [[ "$CVE20146271" =~ "vulnerable" ]]; then
+if [ $CVE20146271 -gt 0 ]; then
 	echo "CVE-2014-6271: VULNERABLE"
 else
 	echo "CVE-2014-6271: not vulnerable"
@@ -11,9 +11,9 @@ fi
 
 
 # CVE-2014-7169
-CVE20147169=$(env X='() { (a)=>\' bash -c "echo echo nonvuln" 2>/dev/null; [[ "$(cat echo 2> /dev/null)" == "nonvuln" ]] && echo "vulnerable" 2> /dev/null)
+CVE20147169=$(env X='() { (a)=>\' bash -c "echo echo nonvuln" 2>/dev/null; [[ "$(cat echo 2> /dev/null)" == "nonvuln" ]] && echo "vulnerable" 2> /dev/null | grep 'vulnerable' | wc -l)
 
-if [[ "$CVE20147169" =~ "vulnerable" ]]; then
+if [ $CVE20147169 -gt 0 ]; then
 	echo "CVE-2014-7169: VULNERABLE"
 else
 	echo "CVE-2014-7169: not vulnerable"
@@ -21,9 +21,9 @@ fi
 
 
 # CVE-2014-7186
-CVE20147186=$(bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' 2>/dev/null || echo "vulnerable")
+CVE20147186=$(bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' 2>/dev/null || echo "vulnerable" | grep 'vulnerable' | wc -l)
 
-if [[ "$CVE20147186" =~ "vulnerable" ]]; then
+if [ $CVE20147186 -gt 0 ]; then
 	echo "CVE-2014-7186: VULNERABLE"
 else
 	echo "CVE-2014-7186: not vulnerable"
@@ -31,9 +31,9 @@ fi
 
 
 # CVE-2014-7187
-CVE20147187=$((for x in {1..200}; do echo "for x$x in ; do :"; done; for x in {1..200}; do echo done; done) | bash || echo "vulnerable")
+CVE20147187=$((for x in {1..200}; do echo "for x$x in ; do :"; done; for x in {1..200}; do echo done; done) | bash || echo "vulnerable" | grep 'vulnerable' | wc -l)
 
-if [[ "$CVE20147187" =~ "vulnerable" ]]; then
+if [ $CVE20147187 -gt 0 ]; then
 	echo "CVE-2014-7186: VULNERABLE"
 else
 	echo "CVE-2014-7186: not vulnerable"
