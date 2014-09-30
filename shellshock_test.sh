32ac2a6469bc977c64b54451566214848ec13aba
TheZ3ro <silvethebest@yahoo.it>
2014-10-01 00:16:02 +0200

    Aded exploit 3 to shellshock_test

--- shellshock_test.sh
+++ shellshock_test.sh
@@ -10,7 +10,6 @@ else
 	echo -e "\033[92mnot vulnerable\033[39m"
 fi
 
-
 # CVE-2014-7169
 CVE20147169=$((cd /tmp; rm -f /tmp/echo; env X='() { (a)=>\' bash -c "echo echo nonvuln" 2>/dev/null; [[ "$(cat echo 2> /dev/null)" == "nonvuln" ]] && echo "vulnerable" 2> /dev/null) | grep 'vulnerable' | wc -l)
 
@@ -21,7 +20,15 @@ else
 	echo -e "\033[92mnot vulnerable\033[39m"
 fi
 
+# CVE-2014-////
+CVE2014=$(env X=' () { }; echo hello' bash -c 'date' | grep 'hello' | wc -l)
 
+echo -n "CVE-2014-//// (exploit 3 on http://shellshocker.net/): "
+if [ $CVE2014 -gt 0 ]; then
+	echo -e "\033[91mVULNERABLE\033[39m"
+else
+	echo -e "\033[92mnot vulnerable\033[39m"
+fi
 
 # CVE-2014-7186
 CVE20147186=$((bash -c 'true <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF <<EOF' 2>/dev/null || echo "vulnerable") | grep 'vulnerable' | wc -l)
@@ -32,8 +39,6 @@ if [ $CVE20147186 -gt 0 ]; then
 else
 	echo -e "\033[92mnot vulnerable\033[39m"
 fi
-
-
 
 # CVE-2014-7187
 CVE20147187=$(((for x in {1..200}; do echo "for x$x in ; do :"; done; for x in {1..200}; do echo done; done) | bash || echo "vulnerable") | grep 'vulnerable' | wc -l)
